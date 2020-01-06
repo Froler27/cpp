3a746c77e064f6863ea0dbc89afd4b5aad545236
Froler <wqpyihu@163.com>
2020-01-06 22:44:58 +0800

    改文件夹名称

---
+++ CppPrimerPlus/chapter two/Q2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int span;
+    cin >> span;
+    cout << span << " long = " << span*220 << " 码" << endl;
+    cin.get();
+
+    return 0;
+}
