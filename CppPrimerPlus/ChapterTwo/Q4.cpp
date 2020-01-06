c456eca4e21faa314334ff047b97491e678aa0e6
Froler <wqpyihu@163.com>
2020-01-06 23:41:32 +0800

    add Q4.cpp

---
+++ CppPrimerPlus/ChapterTwo/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "请输入你的年龄：";
+    cin >> age;
+    cout << "有 " << age * 12 << " 个月" << endl;
+    cin.get();
+    cin.get();
+
+    return 0;
+}
