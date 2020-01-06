3d26240f28f6b444b281c51645db076a91315027
Froler <wqpyihu@163.com>
2020-01-06 19:14:12 +0800

    add chapter two Q1

---
+++ C++ Primer Plus/chapter two/Q1.cpp
@@ -0,0 +1,15 @@
+/**
+ * Q1：编写一个 C++ 程序，它显示您的姓名和地址
+ */
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "name: Froler\n"
+         << "addr: the Gate of the Hell\n"
+         << endl;
+    cin.get();
+
+    return 0;
+}
