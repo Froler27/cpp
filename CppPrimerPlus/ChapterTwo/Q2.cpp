3fba4962078714bc9267367763a34e4c3c9c52a6
Froler <wqpyihu@163.com>
2020-01-06 23:12:13 +0800

    change folder name

--- CppPrimerPlus/ChapterTwo/Q2.cpp
+++ CppPrimerPlus/ChapterTwo/Q2.cpp
@@ -3,9 +3,11 @@
 int main()
 {
     using namespace std;
-    int span;
+    double span;
+    cout << "请输入数字：";
     cin >> span;
     cout << span << " long = " << span*220 << " 码" << endl;
+    cin.get();
     cin.get();
 
     return 0;
